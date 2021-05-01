a63341f466259ff9984717a766c372286ac0a899
Lynn Samuelson <lynn.samuelson@gmail.com>
2021-05-01 13:28:41 -0500

    added dogs

---
+++ dogs.js
@@ -0,0 +1,29 @@
+const string = "Hello World";
+const int = 35;
+const float = 3.78;
+const boolean = true;
+
+const dogNames = ["Joel", "Mr. Tickles", "Daisy", "BenjI", "Nebula", "Snotts", "Rufus"];
+
+const dogInfo = [39.6, "Joel", "Beagle", true, 5];
+
+const joel = {
+  id: 1,
+  weight: 39.6,
+  name: "Joel",
+  breed: "Beagle",
+  goodBoy: true,
+  age: 5
+}
+
+const mrT = {
+  id: 2,
+  weight: 3.2,
+  name: "Mr. Tickles",
+  breed: "Daschund",
+  goodBoy: true,
+  age: 4
+}
+
+console.log(mrT);
+
