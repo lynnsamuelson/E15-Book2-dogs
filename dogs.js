8a978cefacd903a97972dbc72af9477f12aea1dd
simplesnoh <sydneycnoh@gmail.com>
2021-05-01 14:09:44 -0500

    added dogs array of  objects

--- dogs.js
+++ dogs.js
@@ -25,5 +25,46 @@ const mrT = {
   age: 4
 }
 
-console.log(mrT);
+const dogs = [
+  {
+    id: 1,
+    weight: 39.6,
+    name: "Joel",
+    breed: "Beagle",
+    goodBoy: true,
+    age: 5
+  },
+  {
+    id: 2,
+    weight: 3.2,
+    name: "Mr. Tickles",
+    breed: "Daschund",
+    goodBoy: true,
+    age: 4
+  },
+  {
+    id: 3,
+    weight: 4.2,
+    name: "Benji",
+    breed: "Chihuahua",
+    goodBoy: true,
+    age: 0.25
+  },
+  {
+    id: 4,
+    weight: 104.2,
+    name: "Nebula",
+    breed: "Great Dane",
+    goodBoy: false,
+    age: 4
+  }
+]
+
+// console.log(mrT);
+
+// console.log(dogs)
+// console.log(dogs[2])
+// Nebula is a great dane and weighs a hundred pounds
+
+console.log(`${dogs[3].name} is a ${dogs[3].breed} and weighs ${dogs[3].weight} pounds.`)
 
